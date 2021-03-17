eefe17102eb8d8b2aa60078ad91f14dc7ddebcf2
tyagimayank027 <tyagimayank027@gmail.com>
2021-03-17 12:28:19 +0530

    practice question

--- Ex10.java
+++ Ex10.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Ex10 {
 
-	public int[] FindCommonElemet(int[] arr1, int[] arr2) {
+	public void FindCommonElemet(int[] arr1, int[] arr2) {
 		int[] arr3 = new int[20];
 		int a = 0;
 		for (int i = 0; i < arr1.length; i++) {
@@ -10,10 +10,14 @@ public class Ex10 {
 				if (arr1[i] == arr2[j]) {
 					arr3[a] = arr1[i];
 					a++;
+					break;
 				}
 			}
 		}
-		return arr3;
+		for(int b=0;b<arr3.length;b++) {
+			if(arr3[b]!=0)
+			System.out.print(arr3[b]);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -22,7 +26,6 @@ public class Ex10 {
 		int[] arr1 = { 1, 2, 3, 4 };
 
 		int[] arr2 = { 3, 4, 5, 6 };
-
-		System.out.println(c.FindCommonElemet(arr1, arr2));
+		 c.FindCommonElemet(arr1, arr2);
 	}
 }
